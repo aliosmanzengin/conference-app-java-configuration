e256f3279961aa40eb3c8ca77d340ab1ed53930b
aliosmanzengin <aliosmanzengin0212@gmail.com>
2022-04-24 15:10:52 +0300

    Bean Profiles

--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -4,12 +4,16 @@ import com.pluralsight.model.Speaker;
 import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
 import com.pluralsight.repository.SpeakerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
 
+
+// -Dspring.profiles.active=dev
 @Service("speakerService")
+@Profile("dev")
 public class SpeakerServiceImpl implements SpeakerService {
     @Autowired
     private SpeakerRepository repository;
